da024452e5a0d0259a64fa5cb30fb0a36719b378
tlaghzal <132571456+laghzal49@users.noreply.github.com>
2026-09-24 12:51:45 +0100

    Keep Neo-tree dependencies in the canonical workspace config

--- nvim/lua/plugins/workspace.lua
+++ nvim/lua/plugins/workspace.lua
@@ -27,6 +27,8 @@ return {
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim", "nvim-tree/nvim-web-devicons" },
     opts = {
       close_if_last_window = true,
       popup_border_style = "rounded",
